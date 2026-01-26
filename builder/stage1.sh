9d72a5d310586ddb52a5014acbb54ef02e16eacd
YAN Wenkun <code@yanwk.fun>
2026-01-26 10:27:35 +0800

    Update URL comfyanonymous -> Comfy-Org

--- builder/stage1.sh
+++ builder/stage1.sh
@@ -32,8 +32,8 @@ $pip_exe install -r "$workdir"/pak6.txt
 $pip_exe install -r "$workdir"/pak7.txt
 
 # Install comfyui-frontend-package, version determined by ComfyUI release version.
-latest_tag=$(curl -sL https://api.github.com/repos/comfyanonymous/ComfyUI/tags | jq -r '.[0].name')
-$pip_exe install -r "https://github.com/comfyanonymous/ComfyUI/raw/refs/tags/${latest_tag}/requirements.txt"
+latest_tag=$(curl -sL https://api.github.com/repos/Comfy-Org/ComfyUI/tags | jq -r '.[0].name')
+$pip_exe install -r "https://github.com/Comfy-Org/ComfyUI/raw/refs/tags/${latest_tag}/requirements.txt"
 
 $pip_exe install -r "$workdir"/pakA.txt
 $pip_exe install -r "$workdir"/pakP.txt
